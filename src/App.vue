43b30d10c307328e09f734cca8ee49810a5b1c09
Ajay Joshi <joshiajay311@gmail.com>
2023-05-23 10:37:19 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<h1>This is a robo</h1>
+<HomePage />
+<RobotBuilder />
+</template>
+
+<script>
+import RobotBuilder from './build/RobotBuilder';
+import HomePage from './components/HomePage';
+
+export default {
+    name: 'App',
+    components: {
+        HomePage,
+        RobotBuilder
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
+
